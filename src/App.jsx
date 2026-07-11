527bee2c284ac4d9cbe04a24fca567cf459fb8b4
gouthm.04 <gouthm.04@gmail.com>
2026-07-11 10:53:51 +0530

    Integrated Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,21 @@ import AddStudent from './components/AddStudent'
 import SearchStudent from './components/SearchStudent'
 import DeleteStudent from './components/DeleteStudent'
 import ViewStudent from './components/ViewStudent'
+import NavBar from './components/NavBar'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <>
-        <AddStudent/>
-        <SearchStudent/>
-        <DeleteStudent/>
-        <ViewStudent/>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<AddStudent/>}/>
+            <Route path='/delete' element={<DeleteStudent/>}/>
+            <Route path='/search' element={<SearchStudent/>}/>
+            <Route path='/view' element={<ViewStudent/>}/>
+          </Routes>
+        </BrowserRouter>
       </>
   )
 }
--- src/components/ViewStudent.jsx
+++ src/components/ViewStudent.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const ViewStudent = () => {
   return (
     <div>
+        <NavBar/>
         <h1>View Student</h1>
     </div>
   )
